805148e6f2632e5b7f1da69f7f1ff01f9e31231c
as1248 <88760762+as1248@users.noreply.github.com>
2023-08-28 17:09:27 +0900

    Update CreateLecture
    강의 생성할 때 강의 명 유효성 검사 추가

--- src/components/Lecture/CreateLecture/LectureName.js
+++ src/components/Lecture/CreateLecture/LectureName.js
@@ -1,34 +1,41 @@
 import { TextField } from "@mui/material";
+import { useState } from "react";
 import styled from "styled-components";
 
-const LectureName = ({validate, setValidate}) => {
-
-  /* 유효성 검사
-    const validation = (str) => {
-      const reg = /[a-zA-Zㄱ-ㅎ가-힣0-9]+/gim;
-      return reg.test(str);
-    }
-    const nameHandler = (event) => {
-      const value = event.target.value;
-      if(value.length >= 2 && value.length <= 15){
-        for(let i=0;i<value.length;i++){
-          if(validation(value[i])){
-            setValidate(true);
-          }else {
-            setValidate(false);
-            break;
-          }
+const LectureName = () => {
+  const [validate, setValidate] = useState(true);
+  //유효성 검사
+  const validation = (str) => {
+    const reg = /[a-zA-Zㄱ-ㅎ가-힣0-9\s]+/gim;
+    return reg.test(str);
+  };
+  const nameHandler = (event) => {
+    const value = event.target.value;
+    if (value.length >= 2 && value.length <= 50) {
+      for (let i = 0; i < value.length; i++) {
+        if (validation(value[i])) {
+          setValidate(true);
+        } else {
+          setValidate(false);
+          break;
         }
-      } else {
-        setValidate(false);
       }
-    };*/
-    return (
-        <Content>
-            <Title>강의 명</Title>
-            <TextField label='강의 명' helperText='강의 명을 입력해주세요' size="small"/>
-        </Content>
-    );
+    } else {
+      setValidate(false);
+    }
+  };
+  return (
+    <Content>
+      <Title>강의 명</Title>
+      <TextField
+        label="강의 명"
+        onChange={(e)=>nameHandler(e)}
+        helperText="강의 명을 입력해주세요"
+        error={!validate}
+        size="small"
+      />
+    </Content>
+  );
 };
 
 export default LectureName;
