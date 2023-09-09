f15de3d4af41c40e43b3ea9a8a890a13ae497fd7
as1248 <88760762+as1248@users.noreply.github.com>
2023-09-09 18:15:17 +0900

    Update Lecture
    강의 홈 페이지 수정

--- src/components/Lecture/Lecture/LectureHome.js
+++ src/components/Lecture/Lecture/LectureHome.js
@@ -3,24 +3,37 @@ import { useState } from "react";
 import styled from "styled-components";
 
 const LectureHome = () => {
-  const [studentList, setStudentList] = useState([{
-    studentID: 123123
-  },{
-    studentID: 123123
-  },{
-    studentID: 123123
-  },{
-    studentID: 123123
-  },{
-    studentID: 123123
-  },{
-    studentID: 123123
-  }]);
+  const [studentList, setStudentList] = useState([
+    {
+      studentID: 123123,
+    },
+    {
+      studentID: 123124,
+    },
+    {
+      studentID: 123125,
+    },
+    {
+      studentID: 123126,
+    },
+    {
+      studentID: 123127,
+    },
+    {
+      studentID: 123128,
+    },
+  ]);
   return (
-    <Section>
+    <Content>
       <Top>
         <Title>웹 프로그래밍</Title>
-        <Info>컴퓨터 과학 전공자로서 알아야 할 웹프로그래밍 기초 내용으로 클라이언트 중심의 프로그래밍 영역인 HTML, CSS, 자바스크립트를 기본으로 사용하는 법을 배운다. HTML5에 기반을 둔 콘텐츠 작성 방법, 콘텐츠의 스타일을 지정하는 CSS3, 자바스크립트 학습을 통해 동적인 웹페이지를 구축한다. 또한, 자바스크립트를 서버에서 사용하기 위해 node와 express를 학습한다.</Info>
+        <Info>
+          컴퓨터 과학 전공자로서 알아야 할 웹프로그래밍 기초 내용으로 클라이언트
+          중심의 프로그래밍 영역인 HTML, CSS, 자바스크립트를 기본으로 사용하는
+          법을 배운다. HTML5에 기반을 둔 콘텐츠 작성 방법, 콘텐츠의 스타일을
+          지정하는 CSS3, 자바스크립트 학습을 통해 동적인 웹페이지를 구축한다.
+          또한, 자바스크립트를 서버에서 사용하기 위해 node와 express를 학습한다.
+        </Info>
       </Top>
       <Bottom>
         <Title>학생 목록</Title>
@@ -28,27 +41,23 @@ const LectureHome = () => {
           <StudentName>이름</StudentName>
           <StudentID>학번</StudentID>
         </StudentHeader>
-        <List sx={{overflow: 'auto', maxHeight: 300}}>
+        <List sx={{ overflow: "auto", maxHeight: 300 }}>
           {studentList?.map((item) => {
             return (
-              <Student>
-                <StudentName key={item?.name}>
-                  {item?.name}
-                </StudentName>
-                <StudentID key={item?.studentID}>
-                  {item?.studentID}
-                </StudentID>
+              <Student key={item.studentID}>
+                <StudentName>{item?.name}</StudentName>
+                <StudentID>{item?.studentID}</StudentID>
               </Student>
             );
           })}
         </List>
       </Bottom>
-    </Section>
+    </Content>
   );
 };
 export default LectureHome;
 
-const Section = styled.section`
+const Content = styled.div`
   width: 100%;
   padding: 3%;
 `;
--- src/routes/Lecture/Lecture.js
+++ src/routes/Lecture/Lecture.js
@@ -1,30 +1,16 @@
 import styled from "styled-components";
 import Header from "../../components/Header";
-import { useState } from "react";
 import LectureHome from "../../components/Lecture/Lecture/LectureHome";
-import InstanceList from "../../components/Lecture/Lecture/InstanceList";
-import LectureNav from "../../components/Lecture/Lecture/LectureNav";
-import Notices from "../../components/Lecture/Lecture/Notices";
+import LectureNav from "../../components/Lecture/LectureNav";
 
 //관리자일때와 학생일때 구분해서 만들기
 const Lecture = () => {
-  const [nav, setNav] = useState(1);
   return (
     <>
       <Header />
       <Content>
-        <LectureNav setNav={setNav}/>
-        
-        {nav === 1 ? (
-          //강의 홈
-          <LectureHome />
-        ) : nav === 2 ? (
-          // 공지사항
-          <Notices/>
-        ) : (
-          // 인스턴스
-          <InstanceList />
-        )}
+        <LectureNav />
+        <LectureHome />
       </Content>
     </>
   );
@@ -36,41 +22,4 @@ const Content = styled.div`
   width: 100%;
   min-height: 95vh;
   display: flex;
-`;
-
-const Nav = styled.nav`
-  background-color: #4b6586;
-  width: 120px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-const FoldNav = styled(Nav)`
-  width: 60px;
-`;
-
-const NavItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 120px;
-  color: white;
-`;
-
-const LastNavItem = styled(NavItem)`
-  height: 30px;
-  margin-bottom: 20px;
-`;
-
-const Section = styled.section`
-  width: 100%;
-  padding: 4%;
-`;
-
-const Title = styled.div`
-  font-size: 24px;
-  font-weight: 600;
-  margin-bottom: 30px;
 `;
