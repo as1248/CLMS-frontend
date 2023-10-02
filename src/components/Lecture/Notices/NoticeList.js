8d1772b1bd888db2bff38ff4ccee7b61d69dc49d
as1248 <88760762+as1248@users.noreply.github.com>
2023-10-02 18:14:45 +0900

    Update NoticeList
    공지사항 리스트 페이지 수정
    관리자/학생 기능 구분 필요

--- src/components/Lecture/Notices/NoticeList.js
+++ src/components/Lecture/Notices/NoticeList.js
@@ -2,55 +2,67 @@ import styled from "styled-components";
 import { useState } from "react";
 import { List } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import NoticeHeader from "./NoticeHeader";
+import Checkbox from "@mui/material/Checkbox";
+import { useEffect } from "react";
 
 const NoticeList = () => {
   const navigate = useNavigate();
-  const [noticeList, setNoticeList] = useState();
+  const [deleteNotice, setDeleteNotice] = useState(false);
+  const [noticeList, setNoticeList] = useState([
+    {
+      noticeId: 1,
+      title: "공지사항 입니당",
+      lectureId: 1,
+    },
+    {
+      noticeId: 2,
+      title: "공지사항 입니다22",
+      lectureId: 1,
+    },
+    {
+      noticeId: 3,
+      title: "공지사항 입니당33",
+      lectureId: 1,
+    },
+  ]);
+  
+  const [deleteList, setDeleteList] = useState([]);
+  const handleChange = (event) => {
+    const value = event.target.value;
+    if(deleteList.includes(value)){
+      setDeleteList(prev=>prev.filter(i=>i!==value));
+    }else{
+      setDeleteList(prev=>[...prev, value]);
+    }
+  };
+  useEffect(()=>{
+    console.log(deleteList);
+  },[deleteList]);
+
   return (
     <Content>
       <NList>
-        <Title>공지사항</Title>
-        <Notice>
-          <Left>
-            <NoticeTitle>공지 2</NoticeTitle>
-            <NoticeContent>새 공지사항 입니다.</NoticeContent>
-          </Left>
-          <Right>
-            <Date>게시일시 : </Date>
-            <Date>2023년 9월 9일 오후 4:02</Date>
-          </Right>
-        </Notice>
-        <Notice>
-          <Left>
-            <NoticeTitle>공지 1</NoticeTitle>
-            <NoticeContent>공지사항 입니다.</NoticeContent>
-          </Left>
-          <Right>
-            <Date>게시일시 : </Date>
-            <Date>2023년 9월 9일 오후 4:02</Date>
-          </Right>
-        </Notice>
-
-        {/*<List
-          style={{
-            maxHeight: 380,
-            overflow: "auto",
-            border: "1px solid #eaeded",
-          }}
-        >
-          {noticeList?.map((item) => {
-            return (
-              <Notice>
-                <NoticeTitle component="div" key={item?.title}>
-                  {item?.title}
-                </NoticeTitle>
-                <NoticeDate component="div" key={item?.date} onClick={()=>navigate('instanceDetail')}>
-                  {item?.date}
-                </NoticeDate>
-              </Notice>
-            );
-          })}
-        </List>*/}
+      <NoticeHeader setDeleteNotice={setDeleteNotice}/>
+        {<List style={{maxHeight: 600, overflow: "auto"}}>
+            {noticeList?.sort((a,b)=>b.noticeId-a.noticeId)?.map((item) => {
+              return (
+              <Box>
+                {deleteNotice && <Checkbox onChange={handleChange} value={item?.noticeId} style={{marginRight:'20px'}}/>}
+                <Notice key={item?.noticeId}>
+                    <Left onClick={()=>navigate(`${item.noticeId}`)}>
+                      <NoticeTitle>{item.title}</NoticeTitle>
+                      <NoticeContent>새 공지사항 입니다.</NoticeContent>
+                    </Left>
+                  <Right>
+                    <Date>게시일시 : </Date>
+                    <Date>2023년 9월 9일 오후 4:02</Date>
+                  </Right>
+                </Notice>
+              </Box>
+              );
+            })}
+          </List>}
       </NList>
     </Content>
   );
@@ -63,20 +75,20 @@ const Content = styled.div`
   padding: 3%;
 `;
 
-const Title = styled.div`
-  font-size: 24px;
-  font-weight: 600;
-  margin-bottom: 30px;
-`;
-
 const NList = styled.div`
   background-color: white;
   padding: 2%;
+  `;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const Notice = styled.div`
   display: flex;
   justify-content: space-between;
+  width: 100%;
   height: 120px;
   border-bottom: 1px solid black;
   border-top: 1px solid black;
@@ -85,6 +97,7 @@ const Notice = styled.div`
 `;
 
 const Left = styled.div`
+  cursor: pointer;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
