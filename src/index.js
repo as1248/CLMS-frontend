c1452b10a6753215e41b7e4a1da72a4ac233022f
as1248 <88760762+as1248@users.noreply.github.com>
2023-11-20 13:05:33 +0900

    Update Index
    baseUrl 수정

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import axios from "axios";
 import { CookiesProvider } from 'react-cookie';
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://3.39.87.171';
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
