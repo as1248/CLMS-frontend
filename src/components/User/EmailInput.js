ea5b12b0210611ae1fa3b3daf314c4b1b52c96b5
as1248 <88760762+as1248@users.noreply.github.com>
2024-03-16 00:14:11 +0900

    Update Components
    컴포넌트들 경로 변경

--- src/components/User/EmailInput.js
+++ src/components/User/EmailInput.js
@@ -1,11 +1,9 @@
 import TextField from '@mui/material/TextField';
 
 const EmailInput = ({email, setEmail, emailValid, setEmailValid, handleKeyDown, showEmailField}) => {
-
   const handleEmail = (e)=> {
     setEmail(e.target.value);
-    const regex =
-      /^(([^<>()\].,;:\s@"]+(\.[^<>()\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    const regex = /^(([^<>()\].,;:\s@"]+(\.[^<>()\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
     if (regex.test(e.target.value)) {
       setEmailValid(true);
     } else {
