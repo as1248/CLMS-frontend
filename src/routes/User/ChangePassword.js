e4772dfb6810909a49141ad1f857dd68adcf04a3
as1248 <88760762+as1248@users.noreply.github.com>
2024-03-17 00:15:28 +0900

    Update ChangePassword
    비밀번호 변경 페이지 컴포넌트 분리

--- src/routes/User/ChangePassword.js
+++ src/routes/User/ChangePassword.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "@mui/material/Button";
 import styled from 'styled-components'
 import Header from'../../components/Header';
 import Footer from "../../components/Footer";
-import PasswordInput from "../../components/User/PasswordInput";
-import PasswordConfirmInput from "../../components/User/PasswordConfirmInput";
+import SubmitBtn from "../../components/User/SubmitBtn";
+import PasswordField from "../../components/User/PasswordField";
+import PasswordConfirmField from "../../components/User/PasswordConfirmField";
 
 const ChangePassword = () => {
     const navigate = useNavigate(); 
@@ -15,29 +15,6 @@ const ChangePassword = () => {
     const [passwordConfirmValid, setPasswordConfirmValid] = useState(false);
     const [notAllow,setNotAllow] = useState(true);
 
-
-    // 비밀번호가 비밀번호 양식에 맞는지 확인
-    const handlePassword = (e)=> {
-      setPassword(e.target.value);
-      const regex = /^[a-zA-z0-9]{8,20}$/;
-      if(regex.test(e.target.value)) {
-        setPasswordValid(true);
-      } else {
-        setPasswordValid(false);
-      }
-    }
-
-    // 비밀번호 확인이 비밀번호 양식에 맞는지 확인
-    const handlePasswordConfirm = (e)=> {
-      setPasswordConfirm(e.target.value);
-      const regex = password;
-      if(regex===e.target.value) {
-        setPasswordConfirmValid(true);
-      } else {
-        setPasswordConfirmValid(false);
-      }
-    }
-    
     // 비밀번호 변경 버튼 눌렀을 때
     const onClickConfirmButton =() =>{
       if(password!==passwordConfirm){ // 비밀번호와 비밀번호가 일치하지 않을 때
@@ -56,7 +33,6 @@ const ChangePassword = () => {
       }
     }
 
-    //버튼 활성화 실시간으로
     useEffect(() =>{
       if(passwordValid && passwordConfirmValid){
         setNotAllow(false);
@@ -65,34 +41,15 @@ const ChangePassword = () => {
       setNotAllow(true);
     },[passwordValid,passwordConfirmValid]);
     
-    //페이지 UI 설정
     return (
       <>
         <Header/>
         <Container>
         <Box>
           <Title>비밀번호 변경</Title>
-          <PasswordInput password={password} handlePassword={handlePassword} passwordValid={passwordValid} handleKeyDown={handleKeyDown} />
-          {
-            !passwordValid && password.length > 0 && (
-              <StyledText>특문자 제외 영문자 숫자로 8자 이상 20자 미만으로 입력해주세요</StyledText>
-            )
-          }
-          <PasswordConfirmInput passwordConfirm={passwordConfirm} handlePasswordConfirm={handlePasswordConfirm} passwordConfirmValid={passwordConfirmValid} handleKeyDown={handleKeyDown} />
-          {
-            !passwordConfirmValid && passwordConfirm.length > 0 && (
-              <StyledText>비밀번호가 일치하지 않습니다</StyledText>
-            )
-          }
-          <Button
-            disabled={notAllow}
-            onClick={onClickConfirmButton}
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}>
-            비밀번호 변경
-          </Button>
+          <PasswordField password={password} setPassword={setPassword} passwordValid={passwordValid} setPasswordValid={setPasswordValid} handleKeyDown={handleKeyDown}/>
+          <PasswordConfirmField password={password} passwordConfirm={passwordConfirm} setPasswordConfirm={setPasswordConfirm} passwordConfirmValid={passwordConfirmValid} setPasswordConfirmValid={setPasswordConfirmValid} handleKeyDown={handleKeyDown}/>
+          <SubmitBtn notAllow={notAllow} onClickConfirmButton={onClickConfirmButton}>비밀번호 변경</SubmitBtn>
         </Box>
       </Container>
       <Footer/>
@@ -101,11 +58,6 @@ const ChangePassword = () => {
 };
 
 export default ChangePassword;
-
-const StyledText = styled.div`
-color:red;
-font-size:0.8rem;
-`;
 
 const Container = styled.div`
   display: flex;
--- src/routes/User/FindPassword.js
+++ src/routes/User/FindPassword.js
@@ -1,51 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Grid from '@mui/material/Grid';
-import VerifyEmail from '../../components/User/VerifyEmail';
 import Header from'../../components/Header';
 import Footer from "../../components/Footer";
 import styled from "styled-components";
-import EmailInput from "../../components/User/EmailInput";
-import axios from "axios";
-import SendEmailBtn from "../../components/User/SignUp, SignUpAd/SendEmailBtn";
+import EmailField from "../../components/User/EmailField";
+import SubmitBtn from "../../components/User/SubmitBtn";
 
 const FindPassword = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [emailValid, setEmailValid] = useState(false);
   const [notAllow, setNotAllow] = useState(true);
-  const [showEmailField, setShowEmailField] = useState(false);
-  const [NumberValid, setNumberValid] = useState(false); 
-  const [sendButtonDisabled, setSendButtonDisabled] = useState(false);
+  const [authentication, setAuthentication] = useState(false); 
 
-  // 이메일 인증 번호 실시간 조회
   useEffect(() => {
-    if (NumberValid) {
+    if (authentication) {
       setNotAllow(false);
     } else {
       setNotAllow(true);
     }
-  }, [NumberValid]);
+  }, [authentication]);
 
   // 이메일 인증이 완료되면 다음 비밀번호 변경 페이지로 이동
   const onClickConfirmButton = () => {
     navigate('/login/findPassword/changePassword');
-  };
-
-  // 전송 버튼을 클릭했을 때 
-  const handleButtonClick = () => {
-    setShowEmailField(true);
-    setSendButtonDisabled(true);
-    axios.get('/register/verification',
-      { params: { email: email } },
-      { withCredentials: true }
-    ).then(response => {
-
-    }
-    ).catch(error => {
-      console.error(error);
-    });
   };
 
   //Enter가 버튼 클릭 기능으로 구현되도록 설정
@@ -62,24 +40,8 @@ const FindPassword = () => {
         <Box>
           <Title>비밀번호 찾기</Title>
           <SubTitle>비밀번호를 찾고자하는 아이디를 입력해주세요.</SubTitle>
-          <Grid container spacing={2}>
-            <Grid item xs={9}>
-              <EmailInput email={email} setEmail={setEmail} emailValid={emailValid} setEmailValid={setEmailValid} handleKeyDown={handleKeyDown} showEmailField={showEmailField}/>
-            </Grid>
-            <Grid item xs={3}>
-              <SendEmailBtn emailValid={emailValid} sendButtonDisabled={sendButtonDisabled} handleButtonClick={handleButtonClick} />
-            </Grid>
-          </Grid>
-          {showEmailField && <VerifyEmail onNumberValidChange={setNumberValid} />}
-          <Button
-            disabled={notAllow}
-            onClick={onClickConfirmButton}
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}>
-            비밀번호 변경
-          </Button>
+          <EmailField email={email} setEmail={setEmail} emailValid={emailValid} setEmailValid={setEmailValid} handleKeyDown={handleKeyDown} authentication={authentication} setAuthentication={setAuthentication} />
+          <SubmitBtn notAllow={notAllow} onClickConfirmButton={onClickConfirmButton}>비밀번호 변경</SubmitBtn>
         </Box>
       </Container>
       <Footer/>
