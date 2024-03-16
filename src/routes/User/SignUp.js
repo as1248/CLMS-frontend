5885e64b792027eba7f093d9d7f60b7da9e1066c
as1248 <88760762+as1248@users.noreply.github.com>
2024-03-17 00:15:09 +0900

    Update SignUp, SignUpAd
    학습자, 교수자 회원가입 페이지 컴포넌트 분리

--- src/routes/User/SignUp.js
+++ src/routes/User/SignUp.js
@@ -2,24 +2,21 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from'../../components/Header';
 import styled from 'styled-components'
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import VerifyEmail from '../../components/User/VerifyEmail';
 import SelectUniv from '../../components/User/SignUp, SignUpAd/SelectUniv';
 import SelectDept from '../../components/User/SignUp, SignUpAd/SelectDept';
 import axios from 'axios';
 import Footer from "../../components/Footer";
-import EmailInput from "../../components/User/EmailInput";
-import SendEmailBtn from "../../components/User/SignUp, SignUpAd/SendEmailBtn";
-import PasswordInput from "../../components/User/PasswordInput";
-import PasswordConfirmInput from "../../components/User/PasswordConfirmInput";
 import NameInput from "../../components/User/SignUp, SignUpAd/NameInput";
 import StudentNumberInput from "../../components/User/SignUp, SignUpAd/StudentNumberInput";
-import SubmitBtn from "../../components/User/SignUp, SignUpAd/SubmitBtn";
+import SubmitBtn from "../../components/User/SubmitBtn";
+import EmailField from "../../components/User/EmailField";
+import PasswordField from "../../components/User/PasswordField";
+import PasswordConfirmField from "../../components/User/PasswordConfirmField";
 
 const SignUp = () => {
-  const [NumberValid, setNumberValid] = useState(false);
+  const [authentication, setAuthentication] = useState(false);
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -30,36 +27,12 @@ const SignUp = () => {
   const [passwordValid, setPasswordValid] = useState(false);
   const [passwordConfirmValid, setPasswordConfirmValid] = useState(false);
   const [notAllow,setNotAllow] = useState(true);
-  const [sendButtonDisabled, setSendButtonDisabled] = useState(false);
-  const [showEmailField, setShowEmailField] = useState(false);
   const [UnivStu, setUnivStu] = useState('');
   const [DeptStu, setDeptStu] = useState('');
-
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-    const regex =
-      /^[a-zA-z0-9]{8,20}$/;
-    if(regex.test(e.target.value)) {
-      setPasswordValid(true);
-    } else {
-      setPasswordValid(false);
-    }
-  }
-
-  const handlePasswordConfirm = (e) => {
-    setPasswordConfirm(e.target.value);
-    const regex = password;
-    if(regex === e.target.value) {
-      setPasswordConfirmValid(true);
-    } else {
-      setPasswordConfirmValid(false);
-    }
-  }
 
   const handleStudentNumber = (e) => {
     setStudentNumber(e.target.value);
   }
-
 
   const onClickConfirmButton = () => {
     if(password !== passwordConfirm){
@@ -82,27 +55,13 @@ const SignUp = () => {
   }
 
   useEffect(() => {
-    if(NumberValid && passwordValid && passwordConfirmValid && name && studentNumber && UnivStu && DeptStu ){
+    if(authentication && passwordValid && passwordConfirmValid && name && studentNumber && UnivStu && DeptStu ){
       setNotAllow(false);
     } else {
       setNotAllow(true);
     }
-  }, [NumberValid, passwordValid, passwordConfirmValid, name, studentNumber, UnivStu , DeptStu]);
+  }, [authentication, passwordValid, passwordConfirmValid, name, studentNumber, UnivStu , DeptStu]);
 
-    const handleButtonClick = () => {
-      setShowEmailField(true);
-      setSendButtonDisabled(true);
-      axios.get('/register/verification',
-        { params: { email: email } },
-        { withCredentials: true }
-      ).then(response => {
-
-      }
-      ).catch(error => {
-        console.error(error);
-      });
-    };
-  
   return (
     <>
       <Header/>
@@ -116,34 +75,14 @@ const SignUp = () => {
           }}
         >
           <Title>학습자 회원가입</Title>
-          <Grid container spacing={2}>
-            <Grid item xs={9}>
-              <EmailInput email={email} setEmail={setEmail} emailValid={emailValid} setEmailValid={setEmailValid} handleKeyDown={handleKeyDown} showEmailField={showEmailField} />
-              {!emailValid && email.length > 0 && (
-                  <StyledText>올바른 이메일 형식을 입력해주세요</StyledText>
-                )}
-            </Grid>
-            <Grid item xs={3}>
-              <SendEmailBtn emailValid={emailValid} sendButtonDisabled={sendButtonDisabled} handleButtonClick={handleButtonClick} />
-            </Grid>
-          </Grid>
-          {showEmailField && <VerifyEmail email={email} onNumberValidChange={setNumberValid}/>}
-
-          <PasswordInput password={password} handlePassword={handlePassword} passwordValid={passwordValid} handleKeyDown={handleKeyDown} />
-          {!passwordValid && password.length > 0 && (
-              <StyledText>특문자 제외 영문자 숫자로 8자 이상 20자 미만으로 입력해주세요</StyledText>
-            )}
-
-          <PasswordConfirmInput passwordConfirm={passwordConfirm} handlePasswordConfirm={handlePasswordConfirm} passwordConfirmValid={passwordConfirmValid} handleKeyDown={handleKeyDown} />
-          {!passwordConfirmValid && passwordConfirm.length > 0 && (
-              <StyledText>비밀번호가 일치하지 않습니다</StyledText>
-            )}
-
+          <EmailField email={email} setEmail={setEmail} emailValid={emailValid} setEmailValid={setEmailValid} handleKeyDown={handleKeyDown} authentication={authentication} setAuthentication={setAuthentication} />
+          <PasswordField password={password} setPassword={setPassword} passwordValid={passwordValid} setPasswordValid={setPasswordValid} handleKeyDown={handleKeyDown}/>
+          <PasswordConfirmField password={password} passwordConfirm={passwordConfirm} setPasswordConfirm={setPasswordConfirm} passwordConfirmValid={passwordConfirmValid} setPasswordConfirmValid={setPasswordConfirmValid} handleKeyDown={handleKeyDown}/>
           <NameInput name={name} setName={setName} handleKeyDown={handleKeyDown} />
           <StudentNumberInput studentNumber={studentNumber} handleStudentNumber={handleStudentNumber} handleKeyDown={handleKeyDown} />
           <SelectUniv setUnivStu={setUnivStu} />
           <SelectDept universityId={UnivStu} setDeptStu={setDeptStu} />
-          <SubmitBtn notAllow={notAllow} onClickConfirmButton={onClickConfirmButton} />
+          <SubmitBtn notAllow={notAllow} onClickConfirmButton={onClickConfirmButton}>가입하기</SubmitBtn>
         </Box>
       </Container>
       <Footer/>
@@ -152,11 +91,6 @@ const SignUp = () => {
 };
 
 export default SignUp;
-
-const StyledText = styled.div`
-  color:red;
-  font-size:0.8rem;
-`;
 
 const Title = styled.div`
   font-size: 32px;
--- src/routes/User/SignUpAd.js
+++ src/routes/User/SignUpAd.js
@@ -2,24 +2,21 @@ import React, { useEffect, useState } from "react";
 import Header from'../../components/Header';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
 import styled from 'styled-components'
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
-import VerifyEmail from '../../components/User/VerifyEmail';
 import SelectUniv from '../../components/User/SignUp, SignUpAd/SelectUniv';
 import SelectDept from '../../components/User/SignUp, SignUpAd/SelectDept';
 import Footer from "../../components/Footer";
-import EmailInput from "../../components/User/EmailInput";
-import SendEmailBtn from "../../components/User/SignUp, SignUpAd/SendEmailBtn";
-import PasswordInput from "../../components/User/PasswordInput";
-import PasswordConfirmInput from "../../components/User/PasswordConfirmInput";
 import NameInput from "../../components/User/SignUp, SignUpAd/NameInput";
-import SubmitBtn from "../../components/User/SignUp, SignUpAd/SubmitBtn";
+import SubmitBtn from "../../components/User/SubmitBtn";
 import PhoneNumberInput from "../../components/User/SignUp, SignUpAd/PhoneNumberInput";
+import EmailField from "../../components/User/EmailField";
+import PasswordField from "../../components/User/PasswordField";
+import PasswordConfirmField from "../../components/User/PasswordConfirmField";
 
 const SignUpAd = () => {
-    const [NumberValid, setNumberValid] = useState(false);
+    const [authentication, setAuthentication] = useState(false);
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -31,34 +28,9 @@ const SignUpAd = () => {
     const [passwordConfirmValid, setPasswordConfirmValid] = useState(false);
     const [phoneNumberValid, setPhoneNumberValid] = useState(false);
     const [notAllow,setNotAllow] = useState(true);
-    const [sendButtonDisabled, setSendButtonDisabled] = useState(false);
-    const [showEmailField, setShowEmailField] = useState(false);
     const [UnivStu, setUnivStu] = useState('');
     const [DeptStu, setDeptStu] = useState('');
 
-    //비밀번호 오류메세지
-    const handlePassword = (e)=> {
-      setPassword(e.target.value);
-      const regex = /^[a-zA-z0-9]{8,20}$/;
-      if(regex.test(e.target.value)) {
-        setPasswordValid(true);
-      } else {
-        setPasswordValid(false);
-      }
-    }
-
-    //비밀번호 확인 오류메세지
-    const handlePasswordConfirm = (e)=> {
-      setPasswordConfirm(e.target.value);
-      const regex = password;
-      if(regex===e.target.value) {
-        setPasswordConfirmValid(true);
-      } else {
-        setPasswordConfirmValid(false);
-      }
-    }
-
-    //회원가입 버튼 눌렀을 시 메세지
     const onClickConfirmButton =() =>{
       if(password!==passwordConfirm){
         alert('비밀번호를 다시 확인해주세요.');
@@ -82,34 +54,20 @@ const SignUpAd = () => {
       }
     }
 
-    //버튼 활성화 실시간으로
     useEffect(() =>{
-      if(NumberValid && passwordValid && passwordConfirmValid && name && UnivStu && DeptStu && phoneNumberValid ){
+      if(authentication && passwordValid && passwordConfirmValid && name && UnivStu && DeptStu && phoneNumberValid ){
         setNotAllow(false);
         return;
       }
       setNotAllow(true);
-    },[NumberValid,passwordValid,passwordConfirmValid,name,UnivStu,DeptStu,phoneNumberValid]);
-
-    //
-    const handleButtonClick = () => {
-      setShowEmailField(true);
-      setSendButtonDisabled(true);
-      axios.get('/register/verification', { params: { email: email } })
-      .then(response => {
-
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    };
+    },[authentication,passwordValid,passwordConfirmValid,name,UnivStu,DeptStu,phoneNumberValid]);
 
     const handlePhoneNumber = (e) => {
       setPhoneNumber(e.target.value);
       const regex = /^[0-9]{3}-[0-9]{2}-[0-9]{5}$/;
       if (regex.test(e.target.value)) {
           setPhoneNumberValid(true);
-        } else { // 전화번호 아무것도 입력 안 했을 시
+        } else {
           setPhoneNumberValid(false);
         }
       };
@@ -127,34 +85,14 @@ const SignUpAd = () => {
           }}
         >
         <Title>교수자 회원가입</Title>
-        <Grid container spacing={2}>
-          <Grid item xs={9}>
-            <EmailInput email={email} setEmail={setEmail} emailValid={emailValid} setEmailValid={setEmailValid} handleKeyDown={handleKeyDown} showEmailField={showEmailField} />
-            {!emailValid && email.length > 0 && (
-                <StyledText>올바른 이메일 형식을 입력해주세요</StyledText>
-              )}
-          </Grid>
-          <Grid item xs={3}>
-            <SendEmailBtn emailValid={emailValid} sendButtonDisabled={sendButtonDisabled} handleButtonClick={handleButtonClick} />
-          </Grid>
-        </Grid>
-        {showEmailField && <VerifyEmail email={email} onNumberValidChange={setNumberValid}/>}
-
-        <PasswordInput password={password} handlePassword={handlePassword} passwordValid={passwordValid} handleKeyDown={handleKeyDown} />
-        {!passwordValid && password.length > 0 && (
-            <StyledText >특문자 제외 영문자 숫자로 8자 이상 20자 미만으로 입력해주세요</StyledText>
-          )}
-
-        <PasswordConfirmInput passwordConfirm={passwordConfirm} handlePasswordConfirm={handlePasswordConfirm} passwordConfirmValid={passwordConfirmValid} handleKeyDown={handleKeyDown} />
-        {!passwordConfirmValid && passwordConfirm.length > 0 && (
-            <StyledText>비밀번호가 일치하지 않습니다</StyledText>
-          )}
-
+        <EmailField email={email} setEmail={setEmail} emailValid={emailValid} setEmailValid={setEmailValid} handleKeyDown={handleKeyDown} authentication={authentication} setAuthentication={setAuthentication} />
+        <PasswordField password={password} setPassword={setPassword} passwordValid={passwordValid} setPasswordValid={setPasswordValid} handleKeyDown={handleKeyDown}/>
+        <PasswordConfirmField password={password} passwordConfirm={passwordConfirm} setPasswordConfirm={setPasswordConfirm} passwordConfirmValid={passwordConfirmValid} setPasswordConfirmValid={setPasswordConfirmValid} handleKeyDown={handleKeyDown}/>
         <NameInput name={name} setName={setName} handleKeyDown={handleKeyDown} />
         <SelectUniv setUnivStu={setUnivStu} />
         <SelectDept universityId={UnivStu} setDeptStu={setDeptStu} />
         <PhoneNumberInput phoneNumber={phoneNumber} handlePhoneNumber={handlePhoneNumber} phoneNumberValid={phoneNumberValid} handleKeyDown={handleKeyDown} />
-        <SubmitBtn notAllow={notAllow} onClickConfirmButton={onClickConfirmButton} />
+        <SubmitBtn notAllow={notAllow} onClickConfirmButton={onClickConfirmButton}>가입하기</SubmitBtn>
         </Box>
       </Container>
       <Footer/>
@@ -163,11 +101,6 @@ const SignUpAd = () => {
 }
 
 export default SignUpAd;
-
-const StyledText = styled.div`
-color:red;
-font-size:0.8rem;
-`;
 
 const Title = styled.div`
   font-size: 32px;
