83ea1b7d237ac6f8203d47d22c27ac6a0558b4a8
as1248 <88760762+as1248@users.noreply.github.com>
2024-05-30 12:49:59 +0900

    Update Router
    일부 페이지 react.lazy 적용

--- src/Router.js
+++ src/Router.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import Main from "./routes/Main";
 import Login from "./routes/User/Login";
 import Admin from "./routes/Admin";
@@ -9,18 +10,6 @@ import ChangePassword from "./routes/User/ChangePassword";
 import InstanceDetail from "./routes/Instance/InstanceDetail";
 import CreateInstance from "./routes/Instance/CreateInstance";
 import InboundRules from "./routes/Instance/InboundRules";
-import ServerResources from "./routes/Server/ServerResources";
-import CreateServer from "./routes/Server/CreateServer";
-import Lecture from "./routes/Lecture/Lecture";
-import CreateLecture from "./routes/Lecture/CreateLecture";
-import StudentAdministration from "./routes/Lecture/StudentAdministration";
-import LecturesHome from "./routes/Lecture/LecturesHome";
-import Notices from "./routes/Lecture/Notices";
-import Instances from "./routes/Lecture/Instances";
-import NoticeDetail from "./routes/Lecture/NoticeDetail";
-import CreateNotice from "./routes/Lecture/CreateNotice";
-import Enrolment from "./routes/Lecture/Enrolment";
-import ApproveEnrolment from "./routes/Lecture/ApproveEnrolment";
 import Documents from "./routes/Documents";
 import DocsVersions from "./routes/Documents/Info/DocsVersions";
 import DocsGithub from "./routes/Documents/Info/DocsGithub";
@@ -32,46 +21,56 @@ import DocsConnectInstance from "./routes/Documents/Role_User/DocsConnectInstanc
 import DocsCreateInstance from "./routes/Documents/Role_User/DocsCreateInstance";
 import DocsEnrolment from "./routes/Documents/Role_User/DocsEnrolment";
 
+const LecturesHome = lazy(()=>import("./routes/Lecture/LecturesHome"));
+const Enrolment = lazy(()=>import("./routes/Lecture/Enrolment"));
+const CreateServer = lazy(()=>import("./routes/Server/CreateServer"));
+const CreateLecture = lazy(()=>import("./routes/Lecture/CreateLecture"));
+const Lecture = lazy(()=>import("./routes/Lecture/Lecture"));
+const Instances = lazy(()=>import("./routes/Lecture/Instances"));
+const Notices = lazy(()=>import("./routes/Lecture/Notices"));
+const NoticeDetail = lazy(()=>import("./routes/Lecture/NoticeDetail"));
+const CreateNotice = lazy(()=>import("./routes/Lecture/CreateNotice"));
+const ApproveEnrolment = lazy(()=>import("./routes/Lecture/ApproveEnrolment"));
+
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<Main />} />
-        <Route path="/admin" exact element={<Admin />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/login/signUp" exact element={<SignUp />} />
-        <Route path="/login/signUpAd" exact element={<SignUpAd />} />
-        <Route path="/login/findPassword" exact element={<FindPassword />} />
-        <Route path="/login/findPassword/changePassword" exact element={<ChangePassword />} />
+      <Suspense fallback ={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" exact element={<Main />} />
+          <Route path="/admin" exact element={<Admin />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/login/signUp" exact element={<SignUp />} />
+          <Route path="/login/signUpAd" exact element={<SignUpAd />} />
+          <Route path="/login/findPassword" exact element={<FindPassword />} />
+          <Route path="/login/findPassword/changePassword" exact element={<ChangePassword />} />
 
-        <Route path="/documents/" exact element={<Documents />} />
-        <Route path="/documents/versions" exact element={<DocsVersions />} />
-        <Route path="/documents/github" exact element={<DocsGithub />} />
-        <Route path="/documents/contributors" exact element={<DocsContributors/>} />
-        <Route path="/documents/createLecture" exact element={<DocsCreateLecture />} />
-        <Route path="/documents/createServer" exact element={<DocsCreateServer />} />
-        <Route path="/documents/connectDomain" exact element={<DocsConnectDomain />} />
-        <Route path="/documents/connectInstance" exact element={<DocsConnectInstance />} />
-        <Route path="/documents/createInstance" exact element={<DocsCreateInstance/>} />
-        <Route path="/documents/enrolment" exact element={<DocsEnrolment />} />
+          <Route path="/documents/" exact element={<Documents />} />
+          <Route path="/documents/versions" exact element={<DocsVersions />} />
+          <Route path="/documents/github" exact element={<DocsGithub />} />
+          <Route path="/documents/contributors" exact element={<DocsContributors/>} />
+          <Route path="/documents/createLecture" exact element={<DocsCreateLecture />} />
+          <Route path="/documents/createServer" exact element={<DocsCreateServer />} />
+          <Route path="/documents/connectDomain" exact element={<DocsConnectDomain />} />
+          <Route path="/documents/connectInstance" exact element={<DocsConnectInstance />} />
+          <Route path="/documents/createInstance" exact element={<DocsCreateInstance/>} />
+          <Route path="/documents/enrolment" exact element={<DocsEnrolment />} />
 
-
-        <Route path="/studentAdministration" exact element={<StudentAdministration />} />
-        <Route path="/serverResources" exact element={<ServerResources />} />
-        <Route path="/createServer" exact element={<CreateServer />} />
-        <Route path="/createLecture" exact element={<CreateLecture />} />
-        <Route path="/lecturesHome" exact element={<LecturesHome />} />
-        <Route path="/enrolment" exact element={<Enrolment />} />
-        <Route path="/:lectureId" exact element={<Lecture />} />
-        <Route path="/:lectureId/approveEnrolment" exact element={<ApproveEnrolment />} />
-        <Route path="/:lectureId/notice" exact element={<Notices />} />
-        <Route path="/:lectureId/notice/createNotice" exact element={<CreateNotice />} />
-        <Route path="/:lectureId/notice/:noticeId" exact element={<NoticeDetail />} />
-        <Route path="/:lectureId/instances" exact element={<Instances />} />
-        <Route path="/:lectureId/instanceDetail" exact element={<InstanceDetail />} />
-        <Route path="/:lectureId/instanceDetail/inboundRules" exact element={<InboundRules />} />
-        <Route path="/:lectureId/instanceDetail/createInstance" exact element={<CreateInstance />} />
-      </Routes>
+          <Route path="/createServer" exact element={<CreateServer />} />
+          <Route path="/createLecture" exact element={<CreateLecture />} />
+          <Route path="/lecturesHome" exact element={<LecturesHome />} />
+          <Route path="/enrolment" exact element={<Enrolment />} />
+          <Route path="/:lectureId" exact element={<Lecture />} />
+          <Route path="/:lectureId/approveEnrolment" exact element={<ApproveEnrolment />} />
+          <Route path="/:lectureId/notice" exact element={<Notices />} />
+          <Route path="/:lectureId/notice/createNotice" exact element={<CreateNotice />} />
+          <Route path="/:lectureId/notice/:noticeId" exact element={<NoticeDetail />} />
+          <Route path="/:lectureId/instances" exact element={<Instances />} />
+          <Route path="/:lectureId/instanceDetail" exact element={<InstanceDetail />} />
+          <Route path="/:lectureId/instanceDetail/inboundRules" exact element={<InboundRules />} />
+          <Route path="/:lectureId/instanceDetail/createInstance" exact element={<CreateInstance />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
